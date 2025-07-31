845778db8e94d909a493487dcda7d2c8a5117d48
talha485 <talhachaudhary120@gmail.com>
2025-07-31 17:11:12 +0500

    Update 001-find indexes

--- 001-twoSum.py
+++ 001-twoSum.py
@@ -1,6 +1,14 @@
-class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+nums = list(map(int, input("Enter numbers: ").split()))
+target=int(input("Enter target: "))
+found=False
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print([i,j])
+            found=True
+            break
+    if found:
+        break
+if not found:
+    print("No pair found")
+
